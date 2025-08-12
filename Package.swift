81972b73833bd8b31514950c3002b5ebd541abe7
Scott Yelvington <scott@classkick.com>
2025-08-12 13:23:18 -0700

    Adds support via a pre-compiled build for arm64, and arm64 simulator

--- Package.swift
+++ Package.swift
@@ -4,36 +4,15 @@ import PackageDescription
 let package = Package(
     name: "GCUndoManager",
     platforms: [
-        .iOS(.v12),
-        .macOS(.v10_13)
+        .iOS(.v12)
     ],
     products: [
-        .library(
-            name: "GCUndoManager",
-            targets: ["GCUndoManager"]
-        )
+        .library(name: "GCUndoManager", targets: ["GCUndoManager"]) // binary product
     ],
     targets: [
-        .target(
+        .binaryTarget(
             name: "GCUndoManager",
-            path: ".",
-            exclude: [
-                "English.lproj",
-                "GCUndoManagerTestbed.xcodeproj",
-                "GCUndoManagerTestbed-Info.plist",
-                "GCUndoManagerTestbed_Prefix.pch",
-                "GCUndoTestView.h",
-                "GCUndoTestView.m",
-                "MyDocument.h",
-                "MyDocument.m",
-                "main.m",
-                "GCUndoManager.podspec"
-            ],
-            publicHeadersPath: "include",
-            cSettings: [
-                .headerSearchPath("include"),
-                .unsafeFlags(["-fno-objc-arc"], .when(platforms: [.iOS, .macOS]))
-            ]
+            path: "build-xc/GCUndoManager.xcframework"
         )
     ]
 )
