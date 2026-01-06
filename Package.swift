5f0eed12ca9deab4d9924d3dede0204c23454204
Scott Yelvington <scott@classkick.com>
2026-01-05 19:57:21 -0800

    Adds support for macOS, tvOS, and visionOS

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.11
 import PackageDescription
 
 let package = Package(
     name: "GCUndoManager",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v12),
+        .macOS(.v13),
+        .tvOS(.v15),
+        .visionOS(.v1),
     ],
     products: [
         .library(name: "GCUndoManager", targets: ["GCUndoManager"]) // binary product
